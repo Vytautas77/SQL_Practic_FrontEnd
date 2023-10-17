ac6f40bec4c0d406c222a00de3f1d8d552ce5dfd
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-10-17 21:19:13 +0300

    added first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const carWrapper = document.getElementById("carsWrapper");
+const carsFetch = "http://localhost:3000/cars/";
+
+const carCard = (car) => {
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "carWrapper");
+
+  const carTitle = document.createElement("h2");
+  carTitle.setAttribute("class", "carTitle");
+  carTitle.innerHTML = car.title;
+  const carNumberPlates = document.createElement("h5");
+  carNumberPlates.setAttribute("class", "carNumberPlates");
+  carNumberPlates.innerHTML = `Chassis number: ${car.number_plates}`;
+  const carUrl = document.createElement("img");
+  carUrl.setAttribute("class", "carUrl");
+  carUrl.src = car.image;
+  const carPrice = document.createElement("h3");
+  carPrice.setAttribute("class", "carPrice");
+  carPrice.innerHTML = `Price: ${car.price} &#x20AC`;
+
+  const delBtn = document.createElement("button");
+  delBtn.setAttribute("class", "delBtn");
+  delBtn.innerHTML = "DELETE CAR";
+
+  delBtn.addEventListener("click", async () => {
+    try {
+      const carId = car.id;
+      localStorage.setItem("carId", carId);
+      const response = await fetch(carsFetch + carId, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      if (data) {
+        alert("INFORMATION DELETED SUCCESSFULLY.");
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      }
+    } catch (err) {
+      alert("INFORMATION NOT DELETED.");
+    }
+  });
+
+  wrapper.append(carTitle, carNumberPlates, carUrl, carPrice, delBtn);
+
+  return wrapper;
+};
+
+const getCars = async () => {
+  const response = await fetch(carsFetch);
+  const cars = await response.json();
+  cars.cars
+    .sort((a, b) => {
+      return a.title > b.title ? 1 : -1;
+    })
+    .forEach((car) => {
+      const card = carCard(car);
+      carWrapper.append(card);
+    });
+};
+getCars();
---
+++ inputScript.js
@@ -0,0 +1,62 @@
+const AddBtn = document.getElementById("AddBtn");
+const infoMessage = document.getElementById("infoMessage");
+const linkRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+const inputFetch = "http://localhost:3000/cars";
+
+const inputData = () => {
+  const carTitle = document.getElementById("carTitle").value;
+  const carNumberPlates = document.getElementById("carNumberPlates").value;
+  const carPhotoUrl = document.getElementById("carPhotoUrl").value;
+  const carPrice = document.getElementById("carPrice").value;
+  if (!carTitle) {
+    infoMessage.innerHTML = "The Car model field is not entered!";
+    throw new Error("The Car model field is not entered!");
+  }
+  if (!carNumberPlates) {
+    infoMessage.innerHTML = "The Car number plates field is not entered!";
+    throw new Error("The Car number plates field is not entered!");
+  }
+  if (!carPhotoUrl) {
+    infoMessage.innerHTML = "The Car photo URL field is not entered!";
+    throw new Error("The Car photo URL field is not entered!");
+  }
+  if (!linkRegex.test(carPhotoUrl)) {
+    infoMessage.innerHTML = "The Car photo URL invalid track link!";
+    throw new Error("The Car photo URL invalid track link!");
+  }
+  if (!carPrice) {
+    infoMessage.innerHTML = "The Car price field is not entered!";
+    throw new Error("The Car price field is not entered!");
+  }
+
+  const inputs = {
+    title: carTitle,
+    number_plates: carNumberPlates,
+    image: carPhotoUrl,
+    price: carPrice,
+  };
+  return inputs;
+};
+
+AddBtn.addEventListener("click", async () => {
+  const inputCar = inputData();
+  try {
+    const response = await fetch(inputFetch, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputCar),
+    });
+    const data = await response.json();
+    if (data) {
+      infoMessage.innerHTML = "Data uploaded successfully.";
+      setTimeout(() => {
+        window.location.replace("./index.html");
+      }, 5000);
+    }
+  } catch (err) {
+    infoMessage.innerHTML = "Data upload FAILED.";
+  }
+});
